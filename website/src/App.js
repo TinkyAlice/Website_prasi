0a08197f218dab711e115297dad70b0f019cac88
ArAjAlGro <armin.grossmann@sourcetronic.com>
2023-02-02 17:44:34 +0100

    #

--- website/src/App.js
+++ website/src/App.js
@@ -8,15 +8,21 @@ import InfoRout from './InfomationBlatt/InfoRout';
 
 function App() {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-3 h-screen w-full overflow-auto sticky top-0'>
-      <div className='hidden sm:block overflow-auto col-span-2'>
-        <SideBar />
-        <h1>
+    <div className='grid grid-cols-3 h-screen w-full overflow-auto sticky top-0'>
+      <div className=' overflow-auto col-span-2 flex flex-row '>
+        <div class="relative h-full w-full ml-14">
           <Rout/>
-        </h1>
+        </div>
+        <div class="absolute z-10 h-full ">
+          <div className="flex w-full h-full  justify-center">
+            <div className="flex  flex-col w-full h-full text-gray-400 bg-gray-900 rounded z-50">
+              <SideBar/>
+            </div>
+          </div>
+        </div>
       </div>
       <div className='bg-slate-800 grid grid-rows-3 grid-flow-col gap-4 overflow-auto'>
-    	  <InfoRout/>
+        <InfoRout />
       </div>
     </div>
   );
--- website/src/Navi/SideBar.js
+++ website/src/Navi/SideBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import SidebarList from './SidebarList';
+import IconST from '../Bilder/IconST-Fontaine-200x-200x.png'
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(false);
@@ -7,14 +8,14 @@ const Sidebar = () => {
   return (
     <>
       {showSidebar ? (
-        <div class="flex flex-col items-center w-40 h-full overflow-hidden text-gray-400 bg-gray-900 rounded">
+        <div class="flex flex-col items-center w-40 h-full overflow-hidden text-gray-400 bg-gray-900 rounded z-50">
           <button
-            class="flex flex-col items-center w-40 h-full overflow-hidden text-gray-400 bg-gray-900 rounded"
+            class="flex flex-col items-center w-40 h-full overflow-hidden text-gray-400 bg-gray-900 rounded z-50"
             onClick={() => setShowSidebar(!showSidebar)}
           >
 
             <div
-              className={` ${showSidebar ? "translate-x-0 " : "translate-x-full"
+              className={` ${showSidebar ? "translate-x-0 z-50" : "translate-x-full z-50"
                 }`}
             >
               <div class="flex flex-col items-center w-40 h-full overflow-hidden text-gray-400 bg-gray-900 rounded">
@@ -24,10 +25,10 @@ const Sidebar = () => {
           </button>
         </div>
       ) : (
-        <div class="flex flex-col items-center w-16 h-full overflow-hidden text-gray-400 bg-gray-900 rounded">
+        <div class="flex flex-col items-center w-16 h-full overflow-hidden text-gray-400 bg-gray-900 rounded z-50">
           <a class="flex items-center justify-center mt-3" href="#">
-            <svg onClick={() => setShowSidebar(!showSidebar)} class="w-8 h-8 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
+            <svg onClick={() => setShowSidebar(!showSidebar)} class="w-8 h-8 fill-current" xmlns="{IconST}" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M319 1563 c-13 -16 -12 -17 4 -4 9 7 17 15 17 17 0 8 -8 3 -21 -13z" />
             </svg>
           </a>
           <div class="flex flex-col items-center mt-3 border-t border-gray-700">
@@ -79,10 +80,10 @@ const Sidebar = () => {
       )}
 
       <div
-        className={` ${showSidebar ? "translate-x-0 " : "translate-x-full"
+        className={` ${showSidebar ? "translate-x-0 " : "translate-x-full z-50"
           }`}
       >
-        <h3 className="mt-20 text-4xl font-semibold text-gray-300">
+        <h3 className="mt-20 text-4xl font-semibold text-gray-300 z-50">
 
         </h3>
       </div>
